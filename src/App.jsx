693bb65db3bf1360e468a215d8066c878656812c
Abhinash Gupta <avinashguptaa0.3@gmail.com>
2025-02-13 19:21:47 +0545

    new input

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
+      <div className="container">
+        <fieldset className="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box">
+          <legend className="fieldset-legend">Page details</legend>
 
-<fieldset className="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box">
-  <legend className="fieldset-legend">Page details</legend>
-  
-  <label className="fieldset-label">Title</label>
-  <input type="text" className="input" placeholder="My awesome page" />
-  
-  <label className="fieldset-label">Slug</label>
-  <input type="text" className="input" placeholder="my-awesome-page" />
-  
-  <label className="fieldset-label">Author</label>
-  <input type="text" className="input" placeholder="Name" />
-</fieldset>
+          <label className="fieldset-label">Title</label>
+          <input type="text" className="input" placeholder="My awesome page" />
+
+          <label className="fieldset-label">Slug</label>
+          <input type="text" className="input" placeholder="my-awesome-page" />
+
+          <label className="fieldset-label">Author</label>
+          <input type="text" className="input" placeholder="Name" />
+          
+          <label className="fieldset-label">Author</label>
+          <input type="text" className="input" placeholder="Name" />
+        </fieldset>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
