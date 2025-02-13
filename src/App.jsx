e57732b20d2783720f1a2a8f98fe5f291433deac
Abhinash Gupta <avinashguptaa0.3@gmail.com>
2025-02-13 19:18:06 +0545

    table

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,18 @@ function App() {
   return (
     <>
 
-      <h1>Avinash</h1>
+<fieldset className="fieldset w-xs bg-base-200 border border-base-300 p-4 rounded-box">
+  <legend className="fieldset-legend">Page details</legend>
+  
+  <label className="fieldset-label">Title</label>
+  <input type="text" className="input" placeholder="My awesome page" />
+  
+  <label className="fieldset-label">Slug</label>
+  <input type="text" className="input" placeholder="my-awesome-page" />
+  
+  <label className="fieldset-label">Author</label>
+  <input type="text" className="input" placeholder="Name" />
+</fieldset>
     </>
   )
 }
